0b5c2f3859146b48f241d9873a5d170304d0fab0
shwetagavai933 <shwetagavai933@gmail.com>
2021-09-04 23:07:54 +0530

    [Shweta]: Checking Employee Attendance

---
+++ EmployeeWage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeePresence
+{
+    class EmployeeWage
+    {
+        static void Main(string[] args)
+        {
+            
+            int IS_PRESENT_FULL_TIME = 1;
+            Random random = new Random();
+            
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
